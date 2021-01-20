bc3fbb728025c8d51e31536a2d971f26d04263bc
Calvin <c.bauer@nectarpdx.com>
2021-01-19 17:15:17 -0800

    Fixing bugs now

--- static/js/app.js
+++ static/js/app.js
@@ -1,52 +1,43 @@
-// // Add event listener for submit button
-// d3.select("#selDataset").on("change", optionChanged)
 
-
-d3.json("../samples.json").then((importedData) => {
-    var data = importedData;
-    selectBox = d3.select("#selDataset")
-
-    importedData.names.forEach(name => {
-        selectBox.append("option").text(name).attr("value", name)
-    });
-
-
-    console.log(data);
-    console.log(data.names);
-    console.log(data.samples);
-});
-
+function init() {
+    optionChanged()
+}
 
 function optionChanged() {
     d3.json("../samples.json").then((importedData) => {
-        var dropdownMenu = d3.select("#selDataset");
-        var idLookup = dropdownMenu.property("value");
         var data = importedData;
-        var demoInfo = d3.select("#sample-metadata")
-        
-        console.log(idLookup)
-        var filteredmetaData = data.metadata.filter(person => person.id == idLookup)
-        // console.log(filteredmetaData)
+        var dropdownMenu = d3.select("#selDataset");
 
+        data.names.forEach(name => {
+            dropdownMenu.append("option").text(name).attr("value", name)
+        });
+
+        var idLookup = dropdownMenu.property("value");
+        var demoInfo = d3.select("#sample-metadata")
+
+        console.log(idLookup)
+
+        // Filtering Meta Data
+        var filteredmetaData = data.metadata.filter(person => person.id == idLookup)
+        console.log(filteredmetaData)
         cleanButton = filteredmetaData[0].wfreq
 
+        // Filtering Sample Data
         var filteredsampleData = data.samples.filter(person => person.id == idLookup)
         console.log(filteredsampleData[0])
-
         topBacteriaId = filteredsampleData[0].otu_ids.slice(0, 10).reverse();
         topBacteriaValues = filteredsampleData[0].sample_values.slice(0, 10).reverse();
-
         BacteriaId = filteredsampleData[0].otu_ids
         BacteriaValues = filteredsampleData[0].sample_values
         BacteriaLabel = filteredsampleData[0].otu_labels
 
+        // Summary Table
         demoInfo.html("");
-
         Object.entries(filteredmetaData[0]).forEach(([key, value]) => {
             demoInfo.append("p").text(`${key}: ${value}`)
         });
 
-
+        // Bar Graph
         var topOTUrace = [{
             x: topBacteriaValues,
             y: topBacteriaId.map(bact => "OTU" + bact),
@@ -55,9 +46,9 @@ function optionChanged() {
             type: "bar",
             orientation: "h"
         }];
-
         Plotly.newPlot("bar", topOTUrace)
 
+        // Bubble Chart
         var Bubble = {
             y: BacteriaValues,
             x: BacteriaId,
@@ -68,20 +59,16 @@ function optionChanged() {
                 size: BacteriaValues
             }
         };
-
-
         var data = [Bubble];
-
         var layout = {
             title: 'Marker Size and Color',
             showlegend: false,
             height: 600,
             width: 1200
         };
-
         Plotly.newPlot('bubble', data, layout);
 
-
+        // Gauge
         var data = [
             {
                 domain: { x: [0, 1], y: [0, 1] },
@@ -102,8 +89,6 @@ function optionChanged() {
                         { range: [6, 7], color: "blue" },
                         { range: [7, 8], color: "blue" },
                         { range: [8, 9], color: "blue" },
-
-
                     ],
                     threshold: {
                         line: { color: "purple", width: 4 },
@@ -113,32 +98,10 @@ function optionChanged() {
                 }
             }
         ];
-
         var layout = { width: 600, height: 450, margin: { t: 0, b: 0 } };
         Plotly.newPlot('gauge', data, layout);
-
-          
-
-        
-
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     })
 };
 
-
-
+init()
 
