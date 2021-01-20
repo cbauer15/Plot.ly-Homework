f4e3876394a2ae78d3aa244f2c1bece426b64bec
Calvin <c.bauer@nectarpdx.com>
2021-01-19 16:45:57 -0800

    gauge

--- static/js/app.js
+++ static/js/app.js
@@ -28,6 +28,8 @@ function optionChanged() {
         var filteredmetaData = data.metadata.filter(person => person.id == idLookup)
         // console.log(filteredmetaData)
 
+        cleanButton = filteredmetaData[0].wfreq
+
         var filteredsampleData = data.samples.filter(person => person.id == idLookup)
         console.log(filteredsampleData[0])
 
@@ -62,21 +64,72 @@ function optionChanged() {
             text: BacteriaLabel,
             mode: 'markers',
             marker: {
-              color: BacteriaId,
-              size: BacteriaValues
+                color: BacteriaId,
+                size: BacteriaValues
             }
-          };
-          
-          var data = [Bubble];
-          
-          var layout = {
+        };
+
+
+        var data = [Bubble];
+
+        var layout = {
             title: 'Marker Size and Color',
             showlegend: false,
             height: 600,
             width: 1200
-          };
+        };
+
+        Plotly.newPlot('bubble', data, layout);
+
+
+        var data = [
+            {
+                domain: { x: [0, 1], y: [0, 1] },
+                value: cleanButton,
+                title: { text: "Wash Frequency" },
+                type: "indicator",
+                mode: "gauge+number+delta",
+                delta: { reference: 0 },
+                gauge: {
+                    axis: { range: [null, 9] },
+                    steps: [
+                        { range: [0, 1], color: "red" },
+                        { range: [1, 2], color: "orange" },
+                        { range: [2, 3], color: "yellow" },
+                        { range: [3, 4], color: "green" },
+                        { range: [4, 5], color: "blue" },
+                        { range: [5, 6], color: "blue" },
+                        { range: [6, 7], color: "blue" },
+                        { range: [7, 8], color: "blue" },
+                        { range: [8, 9], color: "blue" },
+
+
+                    ],
+                    threshold: {
+                        line: { color: "purple", width: 4 },
+                        thickness: 0.75,
+                        value: 7
+                    }
+                }
+            }
+        ];
+
+        var layout = { width: 600, height: 450, margin: { t: 0, b: 0 } };
+        Plotly.newPlot('gauge', data, layout);
+
           
-          Plotly.newPlot('bubble', data, layout);
+
+        
+
+        
+
+
+
+
+
+
+
+
 
 
 
